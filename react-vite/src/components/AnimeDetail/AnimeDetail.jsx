a38d8a95ae3acbc2682e93829bf4fce0c4bd667a
yokozuna753 <brian894526@gmail.com>
2025-02-27 22:33:09 -0500

    added && statements to missing code AnimeDetail page

--- react-vite/src/components/AnimeDetail/AnimeDetail.jsx
+++ react-vite/src/components/AnimeDetail/AnimeDetail.jsx
@@ -145,7 +145,7 @@ function AnimeDetail() {
                   }}
                 >
                   <h4 style={{ margin: "0 0 10px 0" }}>Select a watchlist:</h4>
-                  {Object.values(watchlists).length > 0 ? (
+                  {watchlists && Object.values(watchlists) && Object.values(watchlists).length > 0 ? (
                     Object.values(watchlists)
                       .filter(
                         (watchlist) =>
@@ -195,18 +195,18 @@ function AnimeDetail() {
             </div>
           </div>
           <div className="anime-extra-info"></div>
-          <p>Likes: {anime_obj.likes}</p>
-          <p>Producers: {anime_obj.producers}</p>
-          <p>Rating: {anime_obj.rating}</p>
-          {anime_obj.trailer_url ? (
+          <p>Likes: {anime_obj && anime_obj.likes && anime_obj.likes}</p>
+          <p>Producers: {anime_obj && anime_obj.producers && anime_obj.producers}</p>
+          <p>Rating: {anime_obj && anime_obj.rating && anime_obj.rating}</p>
+          {anime_obj && anime_obj.trailer_url && anime_obj.trailer_url ? (
             <p>
               Trailer:{" "}
               <a
-                href={`${anime_obj.trailer_url}`}
+                href={`${ anime_obj && anime_obj.trailer_url && anime_obj.trailer_url}`}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                {anime_obj.trailer_url}
+                {anime_obj && anime_obj.trailer_url && anime_obj.trailer_url}
               </a>
             </p>
           ) : (
