439f0803a4291eadb38ece2d93d1c5a390dc61a5
yokozuna753 <brian894526@gmail.com>
2025-07-02 16:16:28 -0400

    added anime genre model and seeder files

--- app/models/anime_genre.py
+++ app/models/anime_genre.py
@@ -1,4 +1,4 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
+from .db import db, environment, SCHEMA
 
 class AnimeGenre(db.Model):
     __tablename__ = 'anime_genres'
@@ -6,6 +6,12 @@ class AnimeGenre(db.Model):
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
-    id = db.Column(db.Integer, primary_key=True)  # mal_id from Jikan
-    name = db.Column(db.String, nullable=False)
-    url = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)  
+    genre_mal_id = db.Column(db.Integer, nullable=False) # mal_id from Jikan
+    genre_name = db.Column(db.String, nullable=False)
+
+    def to_dict(self):
+        return {
+            "genre_mal_id": self.genre_mal_id,
+            "genre_name": self.genre_name,
+        }
